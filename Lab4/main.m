79274746a2e518b7aef3c51d3f008822494805e4
Wilmer Rafael Bandres Hernandez <wbandres@mahisoft.com>
2017-05-29 09:54:17 +0200

    adding new main

--- Lab4/main.m
+++ Lab4/main.m
@@ -1,37 +1,51 @@
-close all;
+files = { 'bricks', 'cells' ,'D3', 'D1', 'chess'};
+patches = [9];
+tolerances = [0.2];
+last = size(files, 2);
+for i=1:last
+   
+    % Input
+    file = string(files(i));
+    file_name = 'data/' + file + '.png';
+    sample = imread(char(file_name));
+    texture_size = [128 128];
+    for j=1: size(patches, 2)
+        for k =1:size(tolerances, 2)
+            close all;
+            fig = figure;
+            patch_size = patches(j); % should be odd (3, 5, 7, etc.)
+            tolerance = tolerances(k);
 
-% Input
-sample = imread('data/blobs.png');
-texture_size = [128 128];
-patch_size = 9; % should be odd (3, 5, 7, etc.)
-tolerance = 0.1;
-
-[texture, copy_map] = synthesize_texture(sample, texture_size, patch_size, tolerance);
-
-% Result
-h1 = subplot(2, 2, 1);
-imshow(sample);
-title('Sample')
-h2 = subplot(2, 2, 2);
-imshow(texture);
-title('Texture')
-h3 = subplot(2, 2, 3);
-imshow(build_color_map([size(sample,1), size(sample,2)]));
-title('Color map')
-h4 = subplot(2, 2, 4);
-imshow(copy_map);
-title('Copy map')
-if size(sample,1) < size(texture,1)
-    size_y = get(h2, 'YLim');
-else
-    size_y = get(h1, 'YLim');
+            [texture, copy_map] = synthesize_texture(sample, texture_size, patch_size, tolerance);
+            
+            % Result
+            h1 = subplot(2, 2, 1);
+            imshow(sample);
+            title('Sample')
+            h2 = subplot(2, 2, 2);
+            imshow(texture);
+            title('Texture')
+            h3 = subplot(2, 2, 3);
+            imshow(build_color_map([size(sample,1), size(sample,2)]));
+            title('Color map')
+            h4 = subplot(2, 2, 4);
+            imshow(copy_map);
+            title('Copy map')
+            if size(sample,1) < size(texture,1)
+                size_y = get(h2, 'YLim');
+            else
+                size_y = get(h1, 'YLim');
+            end
+            if size(sample,2) < size(texture,2)
+                size_x = get(h2, 'XLim');
+            else
+                size_x = get(h1, 'XLim');
+            end
+            set(h1, 'XLim', size_x, 'YLim', size_y);
+            set(h2, 'XLim', size_x, 'YLim', size_y);
+            set(h3, 'XLim', size_x, 'YLim', size_y);
+            set(h4, 'XLim', size_x, 'YLim', size_y);
+            saveas(fig, char('results/' + file + '-' + string(patch_size) + '-' + string(tolerance) + '.png'));
+        end
+    end
 end
-if size(sample,2) < size(texture,2)
-    size_x = get(h2, 'XLim');
-else
-    size_x = get(h1, 'XLim');
-end
-set(h1, 'XLim', size_x, 'YLim', size_y);
-set(h2, 'XLim', size_x, 'YLim', size_y);
-set(h3, 'XLim', size_x, 'YLim', size_y);
-set(h4, 'XLim', size_x, 'YLim', size_y);
